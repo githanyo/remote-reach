38f1d569aa75f24f60fa1d0b3e60badcbba64458
githanyo <jnthnoyo7@gmail.com>
2025-02-28 00:54:37 +0300

    implemented and array

---
+++ start.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    printf("This program handles arrays and will lead to structures");
+    printf("\n let's go!!\n");
+    //code starts here
+    int sdnt [4] = {10,20,30,40};
+    printf("the fourth student number is: %d",sdnt[3]);
+
+
+
+
+
+
+return 0;
+}
